901cb13fb3e0aae907c578bf666365d3c81f418e
Flatshot <jonprapuolenis@gmail.com>
2015-10-09 23:45:33 +0000

    Yes, this will all be on the test

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
 class Question(models.Model):
    question_text = models.CharField(max_length=200)
    pub_date = models.DateTimeField('date published')
@@ -12,7 +13,8 @@ class Question(models.Model):
       return self.question_text
    
    def was_published_recently(self):
-      return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+      now = timezone.now()
+      return now - datetime.timedelta(days=1) <= self.pub_date <= now
    was_published_recently.admin_order_field = 'pub_date'
    was_published_recently.boolean = True
    was_published_recently.short_discription = 'Published recently?'
@@ -24,3 +26,4 @@ class Choice(models.Model):
    
    def __str__(self):
       return self.choice_text
+   
